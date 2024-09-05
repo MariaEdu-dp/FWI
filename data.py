5c4fa20804e3afff5e78789962cc57e70039cb17
MariaEdu-dp <144979698+MariaEdu-dp@users.noreply.github.com>
2024-09-05 13:07:56 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,20 +1,26 @@
 import xarray as xr
+import os
+from cdo import Cdo
 
 """
 Você pode trocar esse dataset pelo maior, de 10GB, que tem todos os dados do FWI, se necessário.
 Para isso, copie e cole o arquivo para a pasta "datasets", clique nele com o botão direito e clique em "Copy Path/Reference".
 Clique em "Path from Content Root, apague o link do xr.open_dataset() e, entre aspas, coloque o link novo.
 """
+def find_nc_file(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".nc"):
+            return os.path.join(directory, filename)
+    raise FileNotFoundError("Nenhum arquivo .nc encontrado no diretório")
 
-data = "datasets/FWI_mensal.nc"
-
+# Encontre o arquivo NetCDF
+data = find_nc_file("datasets")
+print(f"Arquivo encontrado: {data}")
 dataset_xr = xr.open_dataset(data)
 print(dataset_xr)
 
 # Selecione a área de estudo e a variável de interesse
 area = dataset_xr["fwi"].sel(lat=slice(-60, 15), lon=slice(-90, -24))
-
-from cdo import Cdo
 
 # Inicialize a interface CDO
 cdo = Cdo()
